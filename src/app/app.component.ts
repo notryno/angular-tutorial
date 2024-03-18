7c7f09ee133dd448341d7582205b0952948989ae
Ryan <ryanclepcha@gmail.com>
2024-03-18 14:35:32 +0545

    Angular Tutorial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,29 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  imports: [
+    HomeComponent,
+    RouterModule,
+  ],
+  // templateUrl: './app.component.html', 
+  styleUrl: './app.component.scss',
+  template: `
+  <main>
+    <a [routerLink]="['/']">
+      <header class="brand-name">
+        <img class="brand-logo" src="/assets/logo.svg" alt="logo" aria-hidden="true">
+      </header>
+    </a>
+    <section class="content">
+      <router-outlet></router-outlet>
+    </section>
+  </main>
+`,
 })
 export class AppComponent {
   title = 'helloworld';
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,21 @@
+
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { DetailsComponent } from './details/details.component';
 
 export const routes: Routes = [];
+
+const routeConfig: Routes = [
+    {
+      path: '',
+      component: HomeComponent,
+      title: 'Home page'
+    },
+    {
+      path: 'details/:id',
+      component: DetailsComponent,
+      title: 'Home details'
+    }
+  ];
+  
+  export default routeConfig;
